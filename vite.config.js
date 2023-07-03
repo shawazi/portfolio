9857f45f413c600f533a9903cb2757d04f98cd5b
“shawazi” <shawazisonfire@gmail.com>
2023-07-03 06:18:06 -0400

    testing dev vs prod env adjustments in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+const base = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
 })
